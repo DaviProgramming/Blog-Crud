f5b6851d060877affa0f100d314058768ca714c7
David Oliveira <daviddenisson2014@gmail.com>
2022-07-22 03:22:37 -0300

    Front end maybe finish

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ const closeNavbar = () => {
 
 OpenBtn.addEventListener('click', OpenNav);
 closeNav.addEventListener('click', closeNavbar);
+
+const sidebar = document.querySelector('aside');
+const ShowsidebarBtn = document.querySelector('#show__sidebar-btn');
+const HidesidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+const showSidebar =() =>{
+    sidebar.style.left = '0';
+    ShowsidebarBtn.style.display = 'none';
+    HidesidebarBtn.style.display = 'inline-block';
+}
+
+const hidesidebar = () =>{
+    sidebar.style.left = '-100%';
+    ShowsidebarBtn.style.display = 'inline-block';
+    HidesidebarBtn.style.display = 'none';
+}
+
+ShowsidebarBtn.addEventListener('click', showSidebar);
+HidesidebarBtn.addEventListener('click', hidesidebar);
